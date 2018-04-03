7b0341149ff6a3eecff7354a67d8a2d3f3c9b8f0
Clint Wylie <cjwylie@gmail.com>
2018-04-03 11:22:51 -0700

    Coordinator drop segment selection through cost balancer (#5529)
    * drop selection through cost balancer
    
    * use collections.emptyIterator
    
    * add test to ensure does not drop from server with larger loading queue with cost balancer
    
    * javadocs and comments to clear things up
    
    * random drop for completeness

--- server/src/test/java/io/druid/server/coordinator/rules/LoadRuleTest.java
+++ server/src/test/java/io/druid/server/coordinator/rules/LoadRuleTest.java
@@ -410,6 +410,9 @@ public class LoadRuleTest
     final LoadQueuePeon mockPeon = createEmptyPeon();
     mockPeon.dropSegment(EasyMock.anyObject(), EasyMock.anyObject());
     EasyMock.expectLastCall().atLeastOnce();
+    EasyMock.expect(mockBalancerStrategy.pickServersToDrop(EasyMock.anyObject(), EasyMock.anyObject()))
+            .andDelegateTo(balancerStrategy)
+            .times(2);
     EasyMock.replay(throttler, mockPeon, mockBalancerStrategy);
 
     LoadRule rule = createLoadRule(ImmutableMap.of(
@@ -554,7 +557,9 @@ public class LoadRuleTest
     final LoadQueuePeon mockPeon = createEmptyPeon();
     mockPeon.dropSegment(EasyMock.anyObject(), EasyMock.anyObject());
     EasyMock.expectLastCall().atLeastOnce();
-
+    EasyMock.expect(mockBalancerStrategy.pickServersToDrop(EasyMock.anyObject(), EasyMock.anyObject()))
+            .andDelegateTo(balancerStrategy)
+            .times(1);
     EasyMock.replay(throttler, mockPeon, mockBalancerStrategy);
 
     LoadRule rule = createLoadRule(ImmutableMap.of(
